7fc05338b5dacbf738bdd2920339a9a6656eb864
Hemanth11534 <69301082+Hemanth11534@users.noreply.github.com>
2023-02-26 17:17:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket_acl" "example_bucket_acl" {
   bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
