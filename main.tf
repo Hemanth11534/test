90b6bc540b32339f1363de5e15fe13a65d2efd33
Hemanth11534 <69301082+Hemanth11534@users.noreply.github.com>
2023-02-26 17:15:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,8 @@
-resource "aws_s3_bucket" "hemudev" {
-  bucket = "hemu-dev"
+resource "aws_s3_bucket" "example" {
+  bucket = "hemu-bucket"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.hemudev.id
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
-}
-
-resource "aws_s3_bucket_object" "archive" {
-  bucket = aws_s3_bucket.hemudev.id
-  key    = "dev/"
-  content                = "application/x-directory"
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
-  bucket = aws_s3_bucket.hemudev.id
-
-  rule {
-    id      = "dev"
-     status = "Enabled"
-
-    prefix = "dev/"
-	
-    expiration {
-      date = "2022-03-07T00:00:00Z"
-    }
-  }
 }
